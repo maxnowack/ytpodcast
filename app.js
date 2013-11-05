0ddcd94edb075c4c4dd18de5d8a10cd734168332
Max Nowack <max@unsou.de>
2013-11-05 08:36:58 +0100

    added calls to get youtube data

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var app = require('express');
+var express = require('express');
+var app = express();
+var RSS = require('rss');
+var yt = require('youtube-feeds');
 var ytvideos = require('./ytvideos.js');
 
+var generateFeed = function(err,data){
+	console.log(data);
+};
 
 app.get("/playlist/:id",function(req,res){
+	yt.feeds.playlist(req.params.id,{orderby:"published",'max-results':50},generateFeed);
 });
 
-
 app.get("/channel/:id",function(req,res){
+	yt.user.uploads(req.params.id,generateFeed);
 });
 
 app.listen(3000);

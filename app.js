298a4054b272db254d4ca0cd73200244bf5806b8
Max Nowack <max@unsou.de>
2013-11-05 08:35:19 +0100

    added missing slashes

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var app = require('express');
 
-app.get("playlist/:id",function(req,res){
 
+app.get("/playlist/:id",function(req,res){
 });
 
-app.get("channel/:id",function(req,res){
 
+app.get("/channel/:id",function(req,res){
 });
 
 app.listen(3000);

6356ac241566bf2e43a6aa3c62c6df004f2cba50
Max Nowack <max@unsou.de>
2013-11-17 14:02:29 +0100

    fixed bug if no valid id submitted

--- app.js
+++ app.js
@@ -93,14 +93,14 @@ var generateFeed = function(type,data,res){
 app.get("/playlist/:id",function(req,res){
 	yt.feeds.playlist(req.params.id,{orderby:"published",'max-results':50},function(err,data){
 		if(!err) generateFeed('playlist',data,res);
-		else req.send('nope...');
+		else res.send('no playlist with id "' + req.params.id + '"');
 	});
 });
 
 app.get("/channel/:id",function(req,res){
 	yt.user.uploads(req.params.id,function(err,data){
 		if(!err) generateFeed('channel',data,res);
-		else req.send('nope...');
+		else res.send('no channel with id "' + req.params.id + '"');
 	});
 });
 

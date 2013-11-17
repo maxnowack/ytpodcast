decc13b31aa55afb7069166e7b05cb148b389d4c
Max Nowack <max@unsou.de>
2013-11-17 12:53:24 +0100

    changed feed generation to callback method

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ var generateFeed = function(err,data){
 };
 
 app.get("/playlist/:id",function(req,res){
-	yt.feeds.playlist(req.params.id,{orderby:"published",'max-results':50},generateFeed);
+	yt.feeds.playlist(req.params.id,{orderby:"published",'max-results':50},function(err,data){
+		if(!err) generateFeed('playlist',data,res);
+		else req.send('nope...');
+	});
 });
 
 app.get("/channel/:id",function(req,res){
-	yt.user.uploads(req.params.id,generateFeed);
+	yt.user.uploads(req.params.id,function(err,data){
+		if(!err) generateFeed('channel',data,res);
+		else req.send('nope...');
+	});
 });
 
 app.listen(3000);

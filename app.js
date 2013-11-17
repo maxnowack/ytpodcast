edb862f8e7d7e19c4a966bab6880a786b9491bbd
Max Nowack <max@unsou.de>
2013-11-17 12:54:39 +0100

    implemented object mapping

--- app.js
+++ app.js
@@ -3,9 +3,32 @@ var app = express();
 var RSS = require('rss');
 var yt = require('youtube-feeds');
 var ytvideos = require('./ytvideos.js');
+var merge = require('object-mapper').merge;
 
 var generateFeed = function(err,data){
 	console.log(data);
+var videoFormat = "video/mp4";
+var videoQuality = "medium";
+
+var podcastOptionsMap = {
+	"title":"title",
+	"description":"description",
+	"author":"itunesAuthor",
+	"author":"author",
+	"thumbnail.hqDefault":"itunesImage",
+	"thumbnail.hqDefault":"image_url"
+
+};
+var podcastItemsMap = {
+	"item.video.title":"title",
+	"item.video.player.default":"link",
+	"item.id":"guid",
+	"item.video.description":"description",
+	"video.url":"enclosure.url",
+	"item.video.uploaded":"date",
+	"item.author":"itunesAuthor",
+	"item.video.duration":"itunesDuration"
+};
 };
 
 app.get("/playlist/:id",function(req,res){
